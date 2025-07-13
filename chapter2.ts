7a90da87f44a82edca43c76a51b21f943c00e627
chaitanya <chaitanyakhurana0@gmail.com>
2025-07-13 23:52:31 +0530

    extends type  aliases

--- chapter2.ts
+++ chapter2.ts
@@ -1,3 +1,8 @@
+// types aliases 
+type sankhya  = number;
+let n :sankhya;
+
+// interface 
 interface User {
     name : string,
     password :string,
@@ -5,10 +10,10 @@ interface User {
     gender :string,
     phonenumber ? : number
 }
+// extends 
 interface Admin extends User{
     fathersname : string
 }
-
 const fun = (obj:Admin) =>{
     obj.name,
     obj.gender,
@@ -17,8 +22,6 @@ const fun = (obj:Admin) =>{
     obj.phonenumber
     obj.fathersname
 }
-
-
 fun({
     name: 'chaitanya',
     password: 'password',
@@ -26,5 +29,7 @@ fun({
     gender :'male',
     fathersname: 'Abc'
 });
+
+
 
 
